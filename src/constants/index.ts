420f5ed9e3a34b77f1bc2de0ce83af2ee2eda8f7
liji <896126514@qq.com>
2025-06-14 15:21:03 +0800

    feat: editor theme customize

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -172,3 +172,228 @@ export const DEFAULT_FONT_FAMILY_MAP = DEFAULT_FONT_FAMILY_NAMES.reduce(
   },
   {}
 )
+
+// 主题色定义
+export type Theme = 'zinc' | 'slate' | 'stone' | 'gray' | 'neutral' | 'red' | 'rose' | 'orange' | 'green' | 'blue' | 'yellow' | 'violet'
+
+export interface ThemeOption {
+  name: Theme
+  label: string
+  light: {
+    primary: string
+    primaryForeground: string
+  },
+  activeColor: {
+    light: string
+    dark: string
+  },
+  dark: {
+    primary: string
+    primaryForeground: string
+  }
+}
+
+export const THEMES: ThemeOption[] = [
+  {
+    name: 'zinc',
+    label: '锌灰',
+    activeColor: {
+      light: '240 5.9% 10%',
+      dark: '240 5.2% 33.9%',
+    },
+    light: {
+      primary: '240 5.9% 10%',
+      primaryForeground: '0 0% 98%',
+    },
+    dark: {
+      primary: '0 0% 98%',
+      primaryForeground: '240 5.9% 10%',
+    },
+  },
+  {
+    name: 'slate',
+    label: '石板',
+    activeColor: {
+      light: '215.4 16.3% 46.9%',
+      dark: '215.3 19.3% 34.5%',
+    },
+    light: {
+      primary: '215.4 16.3% 46.9%',
+      primaryForeground: '210 40% 98%',
+    },
+    dark: {
+      primary: '210 40% 98%',
+      primaryForeground: '215.4 16.3% 46.9%',
+    },
+  },
+  {
+    name: 'stone',
+    label: '石头',
+    activeColor: {
+      light: '25 5.3% 44.7%',
+      dark: '33.3 5.5% 32.4%',
+    },
+    light: {
+      primary: '25 5.3% 44.7%',
+      primaryForeground: '60 9.1% 97.8%',
+    },
+    dark: {
+      primary: '60 9.1% 97.8%',
+      primaryForeground: '25 5.3% 44.7%',
+    },
+  },
+  {
+    name: 'gray',
+    label: '灰色',
+    activeColor: {
+      light: '220 8.9% 46.1%',
+      dark: '215 13.8% 34.1%',
+    },
+    light: {
+      primary: '220 8.9% 46.1%',
+      primaryForeground: '210 40% 98%',
+    },
+    dark: {
+      primary: '210 40% 98%',
+      primaryForeground: '220 8.9% 46.1%',
+    },
+  },
+  {
+    name: 'neutral',
+    label: '中性',
+    activeColor: {
+      light: '0 0% 45.1%',
+      dark: '0 0% 32.2%',
+    },
+    light: {
+      primary: '0 0% 9%',
+      primaryForeground: '0 0% 98%',
+    },
+    dark: {
+      primary: '0 0% 98%',
+      primaryForeground: '0 0% 9%',
+    },
+  },
+  {
+    name: 'red',
+    label: '红色',
+    activeColor: {
+      light: '0 72.2% 50.6%',
+      dark: '0 72.2% 50.6%',
+    },
+    light: {
+      primary: '0 84.2% 60.2%',
+      primaryForeground: '0 0% 98%',
+    },
+    dark: {
+      primary: '0 84.2% 60.2%',
+      primaryForeground: '0 0% 98%',
+    },
+  },
+  {
+    name: 'rose',
+    label: '玫瑰',
+    activeColor: {
+      light: '346.8 77.2% 49.8%',
+      dark: '346.8 77.2% 49.8%',
+    },
+    light: {
+      primary: '346.8 77.2% 49.8%',
+      primaryForeground: '355.7 100% 97.3%',
+    },
+    dark: {
+      primary: '346.8 77.2% 49.8%',
+      primaryForeground: '355.7 100% 97.3%',
+    },
+  },
+  {
+    name: 'orange',
+    label: '橙色',
+    activeColor: {
+      light: '24.6 95% 53.1%',
+      dark: '20.5 90.2% 48.2%',
+    },
+    light: {
+      primary: '24.6 95% 53.1%',
+      primaryForeground: '60 9.1% 97.8%',
+    },
+    dark: {
+      primary: '20.5 90.2% 48.2%',
+      primaryForeground: '60 9.1% 97.8%',
+    },
+  },
+  {
+    name: 'green',
+    label: '绿色',
+    activeColor: {
+      light: '142.1 76.2% 36.3%',
+      dark: '142.1 70.6% 45.3%',
+    },
+    light: {
+      primary: '142.1 76.2% 36.3%',
+      primaryForeground: '355.7 100% 97.3%',
+    },
+    dark: {
+      primary: '142.1 70.6% 45.3%',
+      primaryForeground: '144.9 80.4% 10%',
+    },
+  },
+  {
+    name: 'blue',
+    label: '蓝色',
+    activeColor: {
+      light: '221.2 83.2% 53.3%',
+      dark: '217.2 91.2% 59.8%',
+    },
+    light: {
+      primary: '221.2 83.2% 53.3%',
+      primaryForeground: '210 40% 98%',
+    },
+    dark: {
+      primary: '217.2 91.2% 59.8%',
+      primaryForeground: '222.2 84% 4.9%',
+    },
+  },
+  {
+    name: 'yellow',
+    label: '黄色',
+    activeColor: {
+      light: '47.9 95.8% 53.1%',
+      dark: '47.9 95.8% 53.1%',
+    },
+    light: {
+      primary: '47.9 95.8% 53.1%',
+      primaryForeground: '26 83.3% 14.1%',
+    },
+    dark: {
+      primary: '47.9 95.8% 53.1%',
+      primaryForeground: '26 83.3% 14.1%',
+    },
+  },
+  {
+    name: 'violet',
+    label: '紫罗兰',
+    activeColor: {
+      light: '262.1 83.3% 57.8%',
+      dark: '263.4 70% 50.4%',
+    },
+    light: {
+      primary: '262.1 83.3% 57.8%',
+      primaryForeground: '210 40% 98%',
+    },
+    dark: {
+      primary: '263.4 70% 50.4%',
+      primaryForeground: '210 40% 98%',
+    },
+  },
+
+]
+
+export const DEFAULT_THEME: Theme = 'zinc'
+
+// Border Radius
+export type BorderRadius = 0 | 0.25 | 0.5 | 0.75 | 1
+
+export const BORDER_RADIUS = [0, 0.25, 0.5, 0.75, 1]
+
+export const DEFAULT_BORDER_RADIUS = 0.5
